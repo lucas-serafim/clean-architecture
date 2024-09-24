c6d02a0b3d2f04f7555c93dea70f61ed0d74505a
Lucas Serafim <lscarvalho4@stefanini.com>
2024-09-24 16:59:19 -0300

    Factory error

--- src/domain/product/validator/product.yup.validation.ts
+++ src/domain/product/validator/product.yup.validation.ts
@@ -6,12 +6,23 @@ export default class ProductYupValidator implements ValidatorInterface<Product> 
 
     validate(entity: Product): void {
         try {
-            yup.object().shape({
+            yup
+            .object()
+            .shape({
                 id: yup.string().required("Id is required"),
                 name: yup.string().required("Name is required"),
                 price: yup.number().moreThan(0, "Price must be greater than zero").required("Price is required")
-
             })
+            .validateSync(
+                {
+                    id: entity.id,
+                    name: entity.name,
+                    price: entity.price
+                },
+                {
+                    abortEarly: false
+                }
+            )
         } catch (errors) {
             const e = errors as yup.ValidationError;
 
